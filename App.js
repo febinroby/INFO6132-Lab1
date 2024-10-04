561598d0d6a071d3cba9ce25beb722a7d3517a3d
febinroby <febinroby@gmail.com>
2024-10-04 13:24:24 -0400

    App.js content added

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title) => {
+    if(title.trim()) {
+      setTasks([...tasks, {
+        id: Date.now().toString(), 
+        title, 
+        status: false
+      }]);
+    }
+  };
+
+  const toggleTaskStatus = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, status: !task.status} : task));
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TaskInput onAddTask={addTask} />
+      <TaskList tasks={tasks} onToggleStatus={toggleTaskStatus} onDeleteTask={deleteTask} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, padding: 16 },
 });
